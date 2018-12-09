03f27d3d98eea806a5dbec221c0fbbb8441fff18
Sally <songqilee@gmail.com>
2018-12-09 16:32:08 -0500

    #10 Show teacher welcome Page

--- src/Person.java
+++ src/Person.java
@@ -4,14 +4,16 @@
  * @author sally
  */
 public class Person {
-
+  
+  protected String type;
   protected String username;
   protected String password;
   protected int number;
 
   // Basically anything that makes sense for a student.
-  public Person(int number, String username, String password) {
+  public Person(int number, String type, String username, String password) {
     this.number = number;
+    this.type = type;
     this.username = username;
     this.password = password;
 
@@ -28,9 +30,20 @@ public class Person {
   public String getPassword() {
     return password;
   }
+  
+
 
   public void setPassword(String password) {
     this.password = password;
+  }
+  
+ public String getType() {
+    return type;
+  }
+ 
+ 
+ public void setType(String type) {
+    this.type = type;
   }
 
   public int getNumber() {
@@ -40,5 +53,7 @@ public class Person {
   public void setNumber(int number) {
     this.number = number;
   }
+  
+
 
 }
