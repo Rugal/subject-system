54dd2e0fe9db3f922fcb2b9b8c647468397605db
Sally Lee <songqi26@outlook.com>
2018-12-13 00:25:22 -0500

    #11 implement admin view all students #6 implement admin add student operation

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@
 import model.Person;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import model.WritingToFiles;
 import view.AdminScreen;
 import view.BaseScreen;
 import view.StudentScreen;
@@ -15,7 +17,7 @@ import view.TeacherScreen;
  */
 public class Main {
 
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws FileNotFoundException, IOException {
     Scanner fileScanner = new Scanner(new File("people"));
     ArrayList<Person> test = new ArrayList<>();
     while (fileScanner.hasNext()) {
@@ -25,7 +27,8 @@ public class Main {
     String Tdata;
     String Udata;
     String Pdata;
-    int num;
+    int numOfType;
+    int numOfOperation;
 
     do {
       System.out.println("Please input Username:");
@@ -59,16 +62,62 @@ public class Main {
     screen.display(Udata);
 
     do {
-      num = scanner.nextInt();
-      screen.selection(num);
-    } while (num != 0);
+      numOfType = scanner.nextInt();
+      screen.selection(numOfType);
+      break;
 
-    System.out.println("You have log out");
+    } while (numOfType != 0);
+
+    if (Tdata.equals("admin")) {
+      numOfOperation = scanner.nextInt();
+      switch (numOfOperation) {
+        case 1:
+          viewSt(test);
+          break;
+        case 2:
+          String u;
+          String p;
+          do {
+            System.out.println("please add student username");
+            u = scanner.next();
+            System.out.println("please add student passward");
+            p = scanner.next();
+            if (verifyStudent(test, "student", u)) {
+              System.out.println("student has already exists");
+            } else {
+              break;
+            }
+          } while (true);
+          WritingToFiles.addStudent(u, p);
+          break;
+
+        default:
+          System.out.println("Please input the right operation");
+          break;
+      }
+    }
+  }
+
+  public static void viewSt(ArrayList<Person> test) {
+    for (int i = 0; i < test.size(); ++i) {
+      if (test.get(i).getType().equals("student")) {
+        System.out.println((test.get(i).getUsername() + test.get(i).getPassword()));
+      }
+    }
   }
 
   public static boolean verify(ArrayList<Person> test, String tdata, String udata, String pdata) {
     for (int i = 0; i < test.size(); ++i) {
       if (test.get(i).getType().equals(tdata) && test.get(i).getUsername().equals(udata) && test.get(i).getPassword().equals(pdata)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean verifyStudent(ArrayList<Person> test, String tdata, String udata) {
+    for (int i = 0; i < test.size(); ++i) {
+      if (test.get(i).getType().equals(tdata) && test.get(i).getUsername().equals(udata)) {
         return true;
       }
     }
